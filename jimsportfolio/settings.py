362a94c765077041475c6c05cd4c9e4c1aa58a5c
jimpfred <jimpfred@yahoo.com>
2021-08-17 13:56:57 -0700

    Heroku adjustments

--- jimsportfolio/settings.py
+++ jimsportfolio/settings.py
@@ -129,3 +129,6 @@ MEDIA_ROOT = BASE_DIR
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
